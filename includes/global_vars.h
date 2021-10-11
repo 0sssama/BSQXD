76766b32f2d0f03cbb668febfdb62cccbc4ab83e
MaherAzzouzi <maherazz04@gmail.com>
2021-10-11 17:16:51 +0100

    Header fix

--- includes/global_vars.h
+++ includes/global_vars.h
@@ -1,8 +1,8 @@
 #ifndef GLOBAL_VARS_H
 # define GLOBAL_VARS_H
 
-// The edge length of the provided square.
-#define N 8
+#define X 27 
+#define Y 9
 
 typedef struct s_square {
     int x;
@@ -10,15 +10,15 @@ typedef struct s_square {
     int l;
 } t_square;
 
-typedef struct board {
+typedef struct s_board {
     int x;
     int y;
     int obs_count;
-    char board[N][N];
+    char bd[X][Y];
 
 } t_board;
 
-
+t_board board;
 t_square current_square;
 
 // EMPTY - OBSTACLE - FULL
